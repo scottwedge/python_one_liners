0447698598f41ffb7041297f4c0eed13a058d785
Scott Wedge <scott.wedge@gmail.com>
2020-11-11 22:53:34 -0500

    Include 'long' way of doing same task

--- 1_Simple_List.py
+++ 1_Simple_List.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-
+# one line way
 lst =  [x**2 for x in range(10)]
 print(lst)
 
+
+# Long way
+lst2 = []  # define list
+for x in range(10):
+  square = x * x
+  lst2.append(square)
+
+print(lst2)
