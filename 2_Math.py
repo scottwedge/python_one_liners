3370e1164576241d409cab4fe54932ee1834e78e
Scott Wedge <scott.wedge@gmail.com>
2020-11-12 20:23:02 -0500

    Python3 acts as expected, python2 of 100/9 gives 11

--- 2_Math.py
+++ 2_Math.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 x,y = 3,2
-
 print("If x=3, y=2")
 print("x/y is: ", x/y)
+
+x,y = 100,9
+
+print("If x,y = 100,9")
+print("int(x)/int(y) is ", int(x)/int(y))
+
+print("float x/y = ", float(x)/y)
 
