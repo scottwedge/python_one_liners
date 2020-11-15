46a1335c1c38ec30d57fb06c6877af82dead0f24
Scott Wedge <scott.wedge@gmail.com>
2020-11-14 22:20:14 -0500

    Ends with

--- 5_Strings.py
+++ 5_Strings.py
@@ -9,3 +9,6 @@ print("all lower:", "MR ME".lower())
 print("all upper", "attention".upper())
 
 print("smartphone starts with 'smart':", "smartphone".startswith("smart"))
+
+print("smartphone ends with 'phone':", "smartphone".endswith("phone"))
+print("smartphone ends with 'Phone':", "smartphone".endswith("Phone"))
