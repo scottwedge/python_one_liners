15962ae004aa9387cd272102216509657b225705
Scott Wedge <scott.wedge@gmail.com>
2020-11-14 22:17:41 -0500

    string starts with

--- 5_Strings.py
+++ 5_Strings.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
-y = "This is lazy\t\n"
+y = "     This is lazy\t\n     "
 print(y)
+print(y.strip())
 
+print("all lower:", "MR ME".lower())
+
+print("all upper", "attention".upper())
+
+print("smartphone starts with 'smart':", "smartphone".startswith("smart"))
