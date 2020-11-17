e7e2066cd4045cb47d7f6a5b7464804a41623f93
Scott Wedge <scott.wedge@gmail.com>
2020-11-16 21:49:31 -0500

    Use insert and concatenation

--- 7_Lists.py
+++ 7_Lists.py
@@ -6,3 +6,14 @@ l.append(1)
 l.append(2)
 print(l," has a length of", len(l))
 
+# Insert
+l.insert(0,-100)
+print(l)
+
+# Concatenate lists
+l1 = [1,2,3]
+l2 = [4,5]
+print(l1)
+print(l2)
+print(l1 + l2)
+
