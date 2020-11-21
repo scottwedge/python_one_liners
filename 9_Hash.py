3c2915a25391b3761a2a69089d579ba1459e22c0
Scott Wedge <scott.wedge@gmail.com>
2020-11-20 21:55:24 -0500

    First set is set of strings

---
+++ 9_Hash.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Sets and hashes
+
+hero = "Harry"
+friend = "Ron"
+heroine = "Hermione"
+
+print(hash(hero))
+print(hash(friend))
+print(hash(heroine))
+
+# Can we create a set of strings?
+set1 = {hero, friend, heroine}
+print(set1)
