10a75fcee715f1c73b42800cd5b0179bdcfa1fd3
Scott Wedge <scott.wedge@gmail.com>
2020-11-20 22:03:14 -0500

    Try but cannot create set of lists

--- 9_Hash.py
+++ 9_Hash.py
@@ -13,3 +13,13 @@ print(hash(heroine))
 # Can we create a set of strings?
 set1 = {hero, friend, heroine}
 print(set1)
+
+
+# Lists
+l1 = [1,2]
+l2 = [3,4]
+l3 = [6,8]
+
+# Try to make a set of lists:
+set2 = {l1, l2, l3}
+print(set2)
