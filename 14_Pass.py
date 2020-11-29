bf4cb3ec3ea263b7023d9985e3d4bc4de7dc6f34
Scott Wedge <scott.wedge@gmail.com>
2020-11-28 21:24:57 -0500

    Loops with 'continue', 'pass' and 'break'

--- 14_Pass.py
+++ 14_Pass.py
@@ -3,7 +3,16 @@
 # Use break, continue and pass statements
 
 j = 1
-while j < 10:
-   print(j)
-   j = j + 1
-   continue
+while j < 100:
+    while j < 30:
+        print(j, "<30")
+        j = j + 2
+        continue
+    while j < 60:
+        print("..",j, "< 60")
+        j = j + 5
+        pass
+    while j < 100:
+        print(j, "<100")
+        j = j + 10
+        break
